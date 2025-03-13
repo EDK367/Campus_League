c87d76f3d2cab86fc4aea92341de43611f456910
EDK367 <denilmonterroso22@gmail.com>
2025-03-12 20:25:40 -0600

    first commit and templeate for backend

---
+++ src/main/java/com/umesdnd/CampusLeague/CampusLeagueApplication.java
@@ -0,0 +1,14 @@
+package com.umesdnd.CampusLeague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// esta clase no se elimina nunca
+@SpringBootApplication
+public class CampusLeagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusLeagueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umesdnd/CampusLeague/CampusLeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umesdnd.CampusLeague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusLeagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
