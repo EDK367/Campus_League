afd7a024c4904213ffd6fc9a851d413184b72ad5
Neyzer538 <124699314+Neyzer538@users.noreply.github.com>
2025-05-18 21:17:04 -0600

    Goal:Creacion del CRUD de goals

---
+++ src/main/java/com/umesdnd/CampusLeague/service/interfaces/GoalServiceInterface.java
@@ -0,0 +1,7 @@
+package com.umesdnd.CampusLeague.service.interfaces;
+
+import com.umesdnd.CampusLeague.model.Goal;
+import com.umesdnd.CampusLeague.service.interfaces.common.CrudServiceInterface;
+
+public interface GoalServiceInterface extends CrudServiceInterface<Goal> {
+}
